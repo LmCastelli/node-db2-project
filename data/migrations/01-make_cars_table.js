c9b981989ecccf68ec00a92a82aa0b255868b9a4
Lucas Castelli <Lucas@Lucass-MacBook-Pro-2.local>
2021-12-07 13:40:09 -0600

    added migration schema, setting up codeGrade

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,26 @@
-exports.up = function (knex) {
-  // DO YOUR MAGIC
+exports.up = async function (knex) {
+  await knex.schema.createTable('cars', table => {
+    table.increments('id')
+
+    table.text('vin',40)
+      .required()
+      .unique()
+    
+    table.text('make',20)
+      .required()
+
+    table.text('model',20)
+      .required()
+
+    table.decimal('mileage',20)
+      .required() 
+    
+    table.text('title',20)
+      
+    table.text('transmission', 20)
+  })
 };
 
-exports.down = function (knex) {
-  // DO YOUR MAGIC
+exports.down = async function (knex) {
+  await knex.schema.dropTableIfExists('cars')
 };
